bf95754327b1e6998a4921a915363f8cd3799650
ldevlnull <numberlnull@gmail.com>
2019-09-12 15:42:14 +0300

    lab1 renamed to lab0; few fixes

--- lab0_function_tabulate/main.c
+++ lab0_function_tabulate/main.c
@@ -3,25 +3,23 @@
 
 const double A = -10;
 const double B = 10;
-const int STEP = 1;
-char *FILE_PATH = "/home/user/Desktop/fei-21/Alex_Ulianov/lab1_funcTabulate/output.txt";
+const double STEP = 1;
+const char *FILE_PATH = "output.txt";
 
 double func(double x){
     return x*x-16;
 }
 
 void saveInFile(){
-    FILE *fp;
-    fp = fopen(FILE_PATH, "w+");
-    for (double x = A; x < B; x+=STEP) {
+    FILE *fp = fopen(FILE_PATH, "w+");
+    for (double x = A; x <= B; x+=STEP) {
         fprintf(fp, "f(%.2f)=%.2f\n", x, func(x));
     }
     fclose(fp);
 }
 
 void printFromFile(){
-    FILE *fp;
-    fp = fopen(FILE_PATH, "r");
+    FILE *fp = fopen(FILE_PATH, "r");
     fseek(fp, 0, SEEK_END);
     long fsize = ftell(fp);
     fseek(fp, 0, SEEK_SET);
@@ -31,8 +29,7 @@ void printFromFile(){
     printf(result);
 }
 
-int main()
-{
+int main(){
     saveInFile();
     printFromFile();
 }
