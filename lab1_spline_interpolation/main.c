5797ce7f8f1d76a604a5f6e2486d32c68cdfabd5
ldevlnull <numberlnull@gmail.com>
2019-09-13 09:58:49 +0300

    calcNodes

--- lab1_spline_interpolation/main.c
+++ lab1_spline_interpolation/main.c
@@ -2,69 +2,58 @@
 #include <math.h>
 #include <stdlib.h>
 
-const double A = 0;
-const double B = 15;
+const double A = 1;
+const double B = 30;
 const char *FILE_PATH = "input.txt";
-const char *FORMAT = "%e %e\n";
+const char *FORMAT = "%f %f\n";
 
 double func(double x) {
-	return 2*sin(x);
+    return 2*sin(x);
 }
 
 void saveInFile() {
-	FILE *fp = fopen(FILE_PATH, "w+");
-	for (double x = A; x <= B; x += 1) {
-		fprintf(fp, FORMAT, x, func(x));
-	}
-	fclose(fp);
+    FILE *fp = fopen(FILE_PATH, "w+");
+    for (double x = A; x <= B; x += 3) {
+        fprintf(fp, FORMAT, x, func(x));
+    }
+    fclose(fp);
 }
 
-void readFromFile(double *a, double *b, double *c, double *d, double *x, double *y) {
-	FILE *fp = fopen(FILE_PATH, "r");
-	char ch;
-	int N = 0;
-	while ((ch = fgetc(fp)) != EOF) {
-		if (ch == '\n')
-			N++;
-	}
-	N--;
-	 a = calloc(sizeof a, N+1);
-	 x = calloc(sizeof x, N+1);
-	double xx[20], yy[20];
-	N--;
-	rewind(fp);
-	//for (int/* i = 0; i < NODES_AMOUNT; i++) {
-	//	fscanf(fp, FORMAT, &xx[i], &yy[i]);
-	//}*/
-	fclose(fp);
-	x = xx;
-	y = yy;
+int countNodes() {
+    FILE *fp = fopen(FILE_PATH, "r");
+    char ch;
+    int N = 0;
+    while ((ch = fgetc(fp)) != EOF)
+        if (ch == '\n')
+            N++;
+    return N;
 }
 
-void calculateCoefficients(double *a, double *b, double *c, double *d, double *x, double *y) {
+void readNodes(float *x, float *y, int N){
+    FILE *fp = fopen(FILE_PATH, "r");
+    for (int i = 0; i < N; i++) {
+        fscanf(fp, FORMAT, &x[i], &y[i]);
+    }
+    fclose(fp);
+}
 
+void calculateCoefficients(double *a, double *b, double *c, double *d, double *x, double *y, int NODES_AMOUNT) {
+    for(int i = 0; i < NODES_AMOUNT; i++){
+        a[i] = x[i];
+    }
 }
 
 int main() {
-	saveInFile();
-	/*double *a = NULL, *b = NULL, *c = NULL, *d = NULL, *x = NULL, *y = NULL;
-	readFromFile(&a, &b, &c, &d, &x, &y);
-	calculateCoefficients(&a, &b, &c, &d, &x, &y);*/
-
-	int N = 0;
-	FILE *data = fopen(FILE_PATH, "r");
-	char ch;
-	while ((ch = fgetc(data)) != EOF) {
-		if (ch == '\n')
-			N++;
-	}
-	double x[20], y[20];
-	rewind(data);
-	for (int i = 0; i < N; i++) {
-		fscanf(data, FORMAT, &x[i], &y[i]);
-	};
-	for (int i = 0; i < N; i++) {
-		printf("%e\n", x[i]);
-	}
-
+//    saveInFile();
+    const NODES_AMOUNT = countNodes();
+    float x[NODES_AMOUNT], y[NODES_AMOUNT];
+    readNodes(&x, &y, NODES_AMOUNT);
+//    for(int i = 0; i < NODES_AMOUNT; i++){
+//        printf("%f\t%f\n", x[i], y[i]);
+//    }
+    float a[NODES_AMOUNT], b[NODES_AMOUNT], c[NODES_AMOUNT], d[NODES_AMOUNT];
+    calculateCoefficients(&a, &b, &c, &d, &x, &y, NODES_AMOUNT);
+    for(int i = 0; i < NODES_AMOUNT; i++){
+        printf("%f\n", a[i]);
+    }
 }
